89c875a4b17b666ebf2824b52fef0e4cf95aca8c
sachin123jadhav <sachin123jadhav@gmail.com>
2024-09-09 10:22:33 +0530

    topba

--- src/navbar/NavHead.jsx
+++ src/navbar/NavHead.jsx
@@ -3,12 +3,21 @@ import img1 from "../assets/Images/goup.png";
 import img3 from "../assets/Images/kumbh logo.png";
 
 const NavHead = ({ lang, dict }) => {
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  const currentDate = new Date().toLocaleDateString("en-GB", options); // Gets today's date in "Monday, 9 September 2024" format
+
   return (
     <div>
       <div className="bg-[#FF5F00] flex gap-4 justify-center md:justify-start col-span-1 text-white">
-        <div className="grid grid-cols-1 md:grid-cols-4 justify-between md:w-11/12 mt-2 xl:w-10/12 2xl:w-9/12 m-auto mb-2">
-          {" "}
-          gdfgfdg
+        <div className="grid grid-cols-1 md:grid-cols-4 justify-between md:w-11/12 mt-1 xl:w-10/12 2xl:w-9/12 m-auto mb-1">
+          <span className="bg-[#EBECED] inline-block max-w-max p-[5px_15px] leading-none text-black text-[14px] rounded-lg py-[10px] px-[15px]">
+            {currentDate}
+          </span>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-4 justify-between md:w-11/12 mt-2 xl:w-10/12 2xl:w-9/12 m-auto mb-2">
