fc9382517859fbe581b8e62068b0eefe44dd85d9
tausif3008~ <tausif3008@gmail.com>
2024-09-24 19:10:31 +0530

    Updated copywrite footer

--- src/footer/Copywrite.jsx
+++ src/footer/Copywrite.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { DICT } from "../urils/dictionary";
+import { DICT } from "../urils/dictionary"; // Assuming you have language support here.
 
 const CopyWrite = ({ lang = "en" }) => {
-  const dict = DICT;
+  const dict = DICT; // Accessing your dictionary for language support.
+
   return (
     <footer className="h-12 w-full flex items-center text-center justify-center bg-orange-400 text-white font-semibold font-roboto">
-      <div className="md:w-10/12 w-9/12 xl:w-10/12 2xl:w-9/12 text- flex items-center justify-center m-auto">
+      <div className="md:w-10/12 w-9/12 xl:w-10/12 2xl:w-9/12 flex flex-col items-center justify-center m-auto">
+        {/* Optional multi-language footer */}
         {/* {dict.last_footer[lang]} */}
-        Copyright © Pryagraj Mela Authority 2024-2025, Developed by Kash IT
-        Solutions
+        <span>Copyright © Prayagraj Mela Authority 2025</span>
+        <small className="text-xs mt-1">
+          Developed by: Kash IT Solutions
+        </small>{" "}
+        {/* Smaller text */}
       </div>
     </footer>
   );
