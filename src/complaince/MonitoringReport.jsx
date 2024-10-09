13c3760a58a6aa082e2a28653106caa3411a8bf0
sudarshanmane <sudarshanmane2110@gmail.com>
2024-10-09 16:01:38 +0530

    done

--- src/complaince/MonitoringReport.jsx
+++ src/complaince/MonitoringReport.jsx
@@ -1,24 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Button, Table, Image, Tag, message, Divider } from "antd";
+import { Button, Table, Image, Divider } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
-import CommonDivider from "../commonComponents/CommonDivider";
 import { ArrowLeftOutlined } from "@ant-design/icons";
-import { useDispatch, useSelector } from "react-redux";
 import { getData } from "../Fetch/Axios";
 import URLS from "../urils/URLS";
 
 const MonitoringReport = ({ data }) => {
   const [details, setDetails] = useState([]);
-  const [questionData, setQuestionData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [assetDetails, setAssetDetails] = useState({});
 
-  const assetInfoSelector = useSelector(
-    (state) => state.monitoringSlice?.assetInfo
-  );
-
   const params = useParams();
-  const dispatch = useDispatch();
 
   const getDetails = async () => {
     setLoading(true);
@@ -126,64 +118,73 @@ const MonitoringReport = ({ data }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="mx-auto px-3 pb-3 bg-white shadow-md rounded-lg w-full mt-3">
-      <div className="flex items-center gap-2 mt-3 font-semibold">
-        <Button
-          className="bg-gray-200 rounded-full w-9 h-9"
-          onClick={() => {
-            navigate("/asset-type-list");
-          }}
-        >
-          <ArrowLeftOutlined></ArrowLeftOutlined>
-        </Button>
-        <div className="text-d9 text-2xl  w-full flex items-end justify-between ">
-          Monitoring Report
-        </div>
-      </div>
-      <Divider className="bg-d9 h-2/3 mt-1"></Divider>
-
-      <div className="mt-3">
-        <div className="flex gap-1 flex-col">
-          <div>
-            Latitude:{" "}
-            <span className="font-semibold">{assetDetails.latitude}</span>
-          </div>
-          <div>
-            Longitude:{" "}
-            <span className="font-semibold">{assetDetails.longitude}</span>
-          </div>
-          <div>
-            Remark: <span className="font-semibold">{assetDetails.remark}</span>
+    <div>
+      <div className="mx-auto px-3 pb-3 bg-white shadow-md rounded-lg w-full mt-3">
+        <div className="flex items-center gap-2 mt-3 font-semibold">
+          <Button
+            className="bg-gray-200 rounded-full w-9 h-9"
+            onClick={() => {
+              navigate("/asset-type-list");
+            }}
+          >
+            <ArrowLeftOutlined></ArrowLeftOutlined>
+          </Button>
+          <div className="text-d9 text-2xl  w-full flex items-end ">
+            <span className="mr-1"> Monitoring Report For: </span>{" "}
+            <span className="text-blue-500"> Asset</span>
           </div>
         </div>
+        <Divider className="bg-d9 h-2/3 mt-1"></Divider>
+        {details.length ? (
+          <div className="mt-3">
+            <div className="flex gap-1 flex-col">
+              <div>
+                Latitude:{" "}
+                <span className="font-semibold">{assetDetails.latitude}</span>
+              </div>
+              <div>
+                Longitude:{" "}
+                <span className="font-semibold">{assetDetails.longitude}</span>
+              </div>
+              <div>
+                Remark:{" "}
+                <span className="font-semibold">{assetDetails.remark}</span>
+              </div>
+            </div>
 
-        <div className="flex justify-between mt-2 mb-3">
-          <div className="flex flex-col text-center font-semibold">
-            <span>QR Code</span>
-            <Image
-              width={130}
-              src={URLS.baseUrl + assetDetails?.photo}
-              alt="QR Code"
+            <div className="flex justify-between mt-2 mb-3">
+              <div className="flex flex-col text-center font-semibold">
+                <span>QR Code</span>
+                <Image
+                  width={130}
+                  src={URLS.baseUrl + assetDetails?.photo}
+                  alt="QR Code"
+                />
+              </div>
+              <div className="flex flex-col text-center font-semibold">
+                <span>Asset Image</span>
+                <Image
+                  width={125}
+                  height={125}
+                  src={URLS.baseUrl + assetDetails?.photo}
+                ></Image>
+              </div>
+            </div>
+            <Table
+              columns={dateColumns}
+              dataSource={details}
+              pagination={false}
+              scroll={{ x: 1200, y: 350 }}
+              bordered
+              className="rounded-none"
+              loading={loading}
             />
           </div>
-          <div className="flex flex-col text-center font-semibold">
-            <span>Asset Image</span>
-            <Image
-              width={125}
-              height={125}
-              src={URLS.baseUrl + assetDetails?.photo}
-            ></Image>
+        ) : (
+          <div className="mt-3 font-semibold text-orange-500 text-center">
+            Not Report Found
           </div>
-        </div>
-        <Table
-          columns={dateColumns}
-          dataSource={details}
-          pagination={false}
-          scroll={{ x: 1200, y: 350 }}
-          bordered
-          className="rounded-none"
-          loading={loading}
-        />
+        )}
       </div>
     </div>
   );
